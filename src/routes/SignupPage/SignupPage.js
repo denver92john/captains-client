574dc14c537bc1b233e0a723cb8e3a2e0e16c10d
Burl Adkins <adkins1393@gmail.com>
2020-01-30 12:01:17 -0500

    SignupPage.js ready

--- src/routes/SignupPage/SignupPage.js
+++ src/routes/SignupPage/SignupPage.js
@@ -1,20 +1,46 @@
 import React, {Component} from 'react';
+import AuthApiService from '../../services/AuthApiService';
 import {Input, Label, Required, Section} from '../../components/Utils/Utils';
 import FormAuth from '../../components/FormAuth/FormAuth';
 
 class SignupPage extends Component {
+    static defaultProps = {
+        history: {
+            push: () => {}
+        }
+    }
+
+    state = {error: null}
+
+    handleRegistrationSuccess = () => {
+        const {history} = this.props;
+        history.push('/login')
+    }
+
     handleSubmit = ev => {
         ev.preventDefault()
+        this.setState({error: null})
         const {username, password, re_password} = ev.target;
         const newUser = {
             username: username.value,
             password: password.value,
             re_password: re_password.value
         }
-        console.log(newUser);
+        //console.log(newUser);
+        AuthApiService.postUser(newUser)
+            .then(() => {
+                username.value = ''
+                password.value = ''
+                re_password.value = ''
+                this.handleRegistrationSuccess()
+            })
+            .catch(res => {
+                this.setState({error: res.error})
+            })
     }
 
     render() {
+        //const {error} = this.state;
         return (
             <div>
                 <Section className="hero">
