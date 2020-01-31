cdba42bcf6bd28c6e3daacde9d672f73ad076fdb
Burl Adkins <adkins1393@gmail.com>
2020-01-31 00:38:38 -0500

    changed List.js to function component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,28 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
 import FormList from '../FormList/FormList';
 import ListItem from '../ListItem/ListItem';
 import './List.css';
 
-class List extends Component {
-    render() {
-        const {items, onPost, onPatch, onDelete} = this.props;
-        return (
-            <div className="wrapper list-component">
-                <FormList onPost={onPost} />
-                <ul className="named-list">
-                    {items.map(item =>
-                        <ListItem 
-                            key={item.id}
-                            item={item}
-                            link={`/list/${item.id}/list-items`}
-                            onPatch={onPatch}
-                            onDelete={onDelete}
-                        />
-                    )}
-                </ul>
-            </div>
-        );
-    }
+function List(props) {
+    const {items, onPost, onPatch, onDelete} = props;
+    return (
+        <div className="wrapper list-component">
+            <FormList onPost={onPost} />
+            <ul className="named-list">
+                {items.map(item =>
+                    <ListItem 
+                        key={item.id}
+                        item={item}
+                        link={`/list/${item.id}/list-items`}
+                        onPatch={onPatch}
+                        onDelete={onDelete}
+                    />
+                )}
+            </ul>
+        </div>
+    );
 }
 
 export default List;
