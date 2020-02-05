c408a5c0b040ddcbf07f41328ce54845e456d1eb
Burl Adkins <adkins1393@gmail.com>
2020-02-05 14:51:58 -0500

    edited BackButton css

--- src/components/BackButton/BackButton.js
+++ src/components/BackButton/BackButton.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import './BackButton.css';
 
 function BackButton(props) {
     return (
-        <div>
+        <div className="backbutton-div">
             <Link to={props.pathTo}>
-                <FontAwesomeIcon icon="arrow-circle-left" />
+                <FontAwesomeIcon icon="arrow-circle-left" className="fa-backbutton-icon" />
             </Link>
         </div>
     );
