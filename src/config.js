fb6cc3ea88b3cf2a3b082a3e3dfa9cb82ec18361
Burl Adkins <adkins1393@gmail.com>
2020-02-04 21:29:24 -0500

    connected to heroku server

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
-    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
+    API_ENDPOINT: 'https://immense-earth-15781.herokuapp.com/api',
     TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'get new auth token'
 }
+
+// 
